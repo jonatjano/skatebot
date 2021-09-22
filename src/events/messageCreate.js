75bd80a5dcc959c813dd5ce33ebc7bffa339ceab
Jonathan Selle <jselle@greensystemes.com>
2021-09-22 14:11:31 +0200

    removed local slash commands registering (was for testing purpose)

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -3,13 +3,13 @@ const {exec} = require("child_process");
 module.exports = message => {
 	watchers.forEach(w => w.newMessage(message))
 
-	if (message.content === "<@!727636604692332616>" || message.content === "<@727636604692332616>") {
-		return message.guild.commands.set([
-			...Object.values(interactions.commandInteraction).map(command => command.command),
-			...Object.values(interactions.contextMenuInteraction).map(command => command.command)
-		])
-			.then(result => message.reply("registered slash commands for current guild"))
-	}
+	// if (message.content === "<@!727636604692332616>" || message.content === "<@727636604692332616>") {
+	// 	return message.guild.commands.set([
+	// 		...Object.values(interactions.commandInteraction).map(command => command.command),
+	// 		...Object.values(interactions.contextMenuInteraction).map(command => command.command)
+	// 	])
+	// 		.then(result => message.reply("registered slash commands for current guild"))
+	// }
 
 	if (message.author.id === "439790095122300928" && message.content === "bot upgrade") {
 		return new Promise((res, rej)  => {
